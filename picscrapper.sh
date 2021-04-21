78f7978cd8f94fdf5ab510f35ea2910aa3384cd2
oscher <oscar@yahoo.es>
2021-04-21 20:06:12 +0200

    Add inventory read loop

--- picscrapper.sh
+++ picscrapper.sh
@@ -13,7 +13,7 @@ DEBUG=$VERBOSITY # Set verbosity level
 INVENTORY_FILE=$1 
 
 [[ $DEBUG -ge 1 ]] && echo El valor de WGET_BIN es $WGET_BIN
-[[ $INVENTORY_FILE -ge 1 ]] && echo El valor de INVENTORY_FILE es $INVENTORY_FILE
+[[ $DEBUG -ge 1 ]] && echo El valor de INVENTORY_FILE es $INVENTORY_FILE
 
 
 
@@ -22,8 +22,13 @@ INVENTORY_FILE=$1
 [[ ! -f $INVENTORY_FILE ]] && (echo Archivo inventario no encontrado && exit 2) 
 
 
-#Main
+# Main
+# Crawl over inventory file
+for LINEA in $(cat $INVENTORY_FILE); do 
+  [[ $DEBUG -ge 1 ]] && echo El valor de LINEA es $LINEA
+  URL=$(echo $LINEA | cut -f1 -d";") # Get url to download
+  NAME=$(echo $LINEA | cut -f2 -d";") # Get file name to save
 
-
+done
 
 
