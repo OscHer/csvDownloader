3c19196207f7133a7c4f8cdb2f755ed076dee6db
oscher <oscar@yahoo.es>
2021-04-21 19:23:56 +0200

    Init WGET_BIN variable

--- picscrapper.sh
+++ picscrapper.sh
@@ -3,3 +3,13 @@
 # Programa para descargar fotos dado un csv separado por ;
 # La primera columna del csv es la url a descargar
 # La segunda columna del csv es el nombre con el que guardaremos la imagen
+
+# Definicion de variables
+WGET_BIN=$(which wget) 
+
+
+
+
+
+#Main
+# Pre requisites or exit
