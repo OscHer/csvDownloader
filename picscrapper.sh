05b17686645d79b6b99c7e14b305225320f4642a
oscher <oscar@yahoo.es>
2021-04-21 19:45:08 +0200

    Set verbosity strategy for the whole application

--- picscrapper.sh
+++ picscrapper.sh
@@ -4,12 +4,22 @@
 # La primera columna del csv es la url a descargar
 # La segunda columna del csv es el nombre con el que guardaremos la imagen
 
-# Definicion de variables
-WGET_BIN=$(which wget) 
 
-echo binario wget $WGET_BIN
+
+# Definicion de variables
+WGET_BIN=$(which wget) # Look for wget binary path
+VERBOSITY=$1 # Get first parameter to set verbosity levelt
+DEBUG=$VERBOSITY # Set verbosity level
+
+[[ $DEBUG -ge 1 ]] && echo El valor de WGET_BIN es $WGET_BIN
 
 
 #Main
 # Pre requisites or exit
 [[ -z $WGET_BIN ]] && (echo Binario de wget no encontrado && exit 1) 
+
+
+
+
+
+
